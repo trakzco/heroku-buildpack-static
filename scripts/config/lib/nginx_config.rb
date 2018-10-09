d66e903526fd4d79f9ad3efb8f2ee32db19d9dc6
trakzco <43733379+trakzco@users.noreply.github.com>
2018-10-09 17:45:50 -0400

    Update nginx_config.rb

--- scripts/config/lib/nginx_config.rb
+++ scripts/config/lib/nginx_config.rb
@@ -8,12 +8,8 @@ class NginxConfig
     encoding: "UTF-8",
     clean_urls: false,
     https_only: false,
-    worker_connections: 512,
-    resolver: "8.8.8.8",
-    logging: {
-      "access" => true,
-      "error" => "error"
-    }
+    max_body_size: "1M",
+    worker_connections: 512
   }
 
   def initialize(json_file)
@@ -23,27 +19,22 @@ class NginxConfig
     json["port"] ||= ENV["PORT"] || 5000
     json["root"] ||= DEFAULT[:root]
     json["encoding"] ||= DEFAULT[:encoding]
-
-    index = 0
     json["proxies"] ||= {}
     json["proxies"].each do |loc, hash|
       evaled_origin = NginxConfigUtil.interpolate(hash['origin'], ENV)
-      uri           = URI(evaled_origin)
-
-      json["proxies"][loc]["name"] = "upstream_endpoint_#{index}"
-      cleaned_path = uri.path
-      cleaned_path.chop! if cleaned_path.end_with?("/")
-      json["proxies"][loc]["path"] = cleaned_path
-      json["proxies"][loc]["host"] = uri.dup.tap {|u| u.path = '' }.to_s
-      %w(http https).each do |scheme|
-        json["proxies"][loc]["redirect_#{scheme}"] = uri.dup.tap {|u| u.scheme = scheme }.to_s
-        json["proxies"][loc]["redirect_#{scheme}"] += "/" if !uri.to_s.end_with?("/")
+      if evaled_origin != "/"
+        json["proxies"][loc].merge!("origin" => evaled_origin + "/")
       end
-      index += 1
+
+      uri = URI(evaled_origin)
+      json["proxies"][loc]["path"] = uri.path
+      uri.path = ""
+      json["proxies"][loc]["host"] = uri.to_s
     end
 
     json["clean_urls"] ||= DEFAULT[:clean_urls]
     json["https_only"] ||= DEFAULT[:https_only]
+    json["max_body_size"] ||= DEFAULT[:max_body_size]
 
     json["routes"] ||= {}
     json["routes"] = NginxConfigUtil.parse_routes(json["routes"])
@@ -54,21 +45,7 @@ class NginxConfig
     end
 
     json["error_page"] ||= nil
-    json["debug"] = ENV['STATIC_DEBUG']
-
-    logging = json["logging"] || {}
-    json["logging"] = DEFAULT[:logging].merge(logging)
-
-    nameservers = []
-    if File.exist?("/etc/resolv.conf")
-      File.open("/etc/resolv.conf", "r").each do |line|
-        next unless md = line.match(/^nameserver\s*(\S*)/)
-        nameservers << md[1]
-      end
-    end
-    nameservers << [DEFAULT[:resolver]] unless nameservers.empty?
-    json["resolver"] = nameservers.join(" ")
-
+    json["debug"] ||= ENV['STATIC_DEBUG']
     json.each do |key, value|
       self.class.send(:define_method, key) { value }
     end
